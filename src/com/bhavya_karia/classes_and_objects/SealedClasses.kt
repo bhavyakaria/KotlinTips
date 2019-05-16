31a8cd4157a82d144688ef1939bb0b0a6bb06812
Bhavya Karia <bhavyakaria@yahoo.in>
2019-05-16 18:30:12 +0530

    added sealed classes code

--- src/com/bhavya_karia/classes_and_objects/SealedClasses.kt
+++ src/com/bhavya_karia/classes_and_objects/SealedClasses.kt
@@ -4,6 +4,31 @@ package com.bhavya_karia.classes_and_objects
  * @author Bhavya Karia
  */
 
-fun main() {
+/**
+ * Example of sealed classes
+ */
 
+sealed class SealedClasses {
+    val sampleValue = "Sample Value"
 }
+
+class ClassA : SealedClasses()
+
+class ClassB : SealedClasses()
+
+
+fun main() {
+    val classA: ClassA = ClassA()
+
+    checkSealedClasses(classA)
+}
+
+fun checkSealedClasses(aClass: SealedClasses) {
+    when (aClass) {
+        is ClassA -> println("It's a class A")
+        is ClassB -> println("It's a class B")
+    }
+}
+
+
+
